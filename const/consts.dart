daa7b945d96ea0ce013b6a7a5f2120cbb0ec8e9d
Nirav Kakadiya <niravkakdia@gmail.com>
2024-05-01 15:52:03 +0530

    const

---
+++ const/consts.dart
@@ -0,0 +1,20 @@
+export 'package:flutter/material.dart';
+export 'package:get/get.dart';
+// export 'package:lottie/lottie.dart';
+export 'package:velocity_x/velocity_x.dart';
+// export 'package:video_player/video_player.dart';
+
+export '../widgets/NeuBox.dart';
+
+export '../view/Chat.dart';
+export '../view/About.dart';
+export '../view/LogOut.dart';
+export '../view/PrivacyPolicy.dart';
+export '../view/Support.dart';
+export '../view/TermsAndConditions.dart';
+export '../view/TimeTable.dart';
+export '../view/gallery.dart';
+export '../view/homework.dart';
+export '../view/result.dart';
+export '../widgets/Drawer.dart';
+export 'constants.dart';
